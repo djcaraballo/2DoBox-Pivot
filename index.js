3eaf43c874b659ecbdb9d23c595b79df9d627e11
Dina <dcaraballo26@gmail.com>
2018-07-20 17:51:47 -0600

    Add comments to js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// remove global variables
+// refactor code flow
+// create objects
 var title = $('#title-input').val();
 var body = $('#body-input').val();
 var numCards = 0;
 var qualityVariable = "swill";
 
+// refactor with template literal
+// use expression interpolation
+// change ids to classes across all files
 var newCard = function(id , title , body , quality) {
     return '<div id="' + id + '"class="card-container"><h2 class="title-of-card">'  
             + title +  '</h2>'
@@ -46,11 +52,11 @@ $('.save-btn').on('click', function(event) {
     localStoreCard();
     $('form')[0].reset();
 });
-
+// refactor toggling of buttons
 $(".bottom-box").on('click', function(event){
     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
     var qualityVariable;
-
+// remove nested if/else statements - possibly use ternary operators
     if (event.target.className === "upvote" || event.target.className === "downvote"){
 
         if (event.target.className === "upvote" && currentQuality === "plausible"){
@@ -75,7 +81,6 @@ $(".bottom-box").on('click', function(event){
         } else if (event.target.className === "upvote" && currentQuality === "genius") {
             qualityVariable = "genius";
         }
-
     var cardHTML = $(event.target).closest('.card-container');
     var cardHTMLId = cardHTML[0].id;
     var cardObjectInJSON = localStorage.getItem(cardHTMLId);
