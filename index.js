9a8171051680a711107f859073fc962e747abae8
Dina <dcaraballo26@gmail.com>
2018-07-21 14:29:54 -0600

    Refactor js syntax

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // remove global variables
 // refactor code flow
 // create objects
+// refactor ids to classes when possible - already refactored css and html
 var title = $('#title-input').val();
 var body = $('#body-input').val();
 var numCards = 0;
@@ -8,7 +9,6 @@ var qualityVariable = "swill";
 
 // refactor with template literal
 // use expression interpolation
-// change ids to classes across all files
 var newCard = function(id , title , body , quality) {
     return '<div id="' + id + '"class="card-container"><h2 class="title-of-card">'  
             + title +  '</h2>'
@@ -46,7 +46,6 @@ $('.save-btn').on('click', function(event) {
     if ($('#title-input').val() === "" || $('#body-input').val() === "") {
        return false;
     };  
-
     numCards++;
     $( ".bottom-box" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
     localStoreCard();
@@ -58,26 +57,20 @@ $(".bottom-box").on('click', function(event){
     var qualityVariable;
 // remove nested if/else statements - possibly use ternary operators
     if (event.target.className === "upvote" || event.target.className === "downvote"){
-
         if (event.target.className === "upvote" && currentQuality === "plausible"){
             qualityVariable = "genius";
             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
         } else if (event.target.className === "upvote" && currentQuality === "swill") {
             qualityVariable = "plausible";
-            $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
+            $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);            
         } else if (event.target.className === "downvote" && currentQuality === "plausible") {
             qualityVariable = "swill"
             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
         } else if (event.target.className === "downvote" && currentQuality === "genius") {
             qualityVariable = "plausible"
             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
         } else if (event.target.className === "downvote" && currentQuality === "swill") {
-            qualityVariable = "swill";
-        
+            qualityVariable = "swill";        
         } else if (event.target.className === "upvote" && currentQuality === "genius") {
             qualityVariable = "genius";
         }
@@ -85,13 +78,10 @@ $(".bottom-box").on('click', function(event){
     var cardHTMLId = cardHTML[0].id;
     var cardObjectInJSON = localStorage.getItem(cardHTMLId);
     var cardObjectInJS = JSON.parse(cardObjectInJSON);
-
     cardObjectInJS.quality = qualityVariable;
-
     var newCardJSON = JSON.stringify(cardObjectInJS);
     localStorage.setItem(cardHTMLId, newCardJSON);
-    }
-   
+    }   
     else if (event.target.className === "delete-button") {
         var cardHTML = $(event.target).closest('.card-container').remove();
         var cardHTMLId = cardHTML[0].id;
